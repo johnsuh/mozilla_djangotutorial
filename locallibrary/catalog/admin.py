6a6f8df429ab87535a6df89bdb08195404c6041a
johnsuh <john.suh0@gmail.com>
2018-07-27 10:34:44 -0400

    mozilla tutorial part 6

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import Book,Author,Genre,BookInstance,Language
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+admin.site.register(Book, BookAdmin)
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+admin.site.register(Author, AuthorAdmin)
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+admin.site.register(Genre)
+admin.site.register(Language)
